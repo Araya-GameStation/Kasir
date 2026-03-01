bcdbd6a28b8c6d6ee8141f0fdf7dd8bce4d053bb
Araya-GameStation <yzaxx.77@gmail.com>
2026-03-01 14:29:34 +0800

    Delete utils.js

--- utils.js
+++
@@ -1,9 +0,0 @@
-
-function formatRupiah(num){return "Rp "+num.toLocaleString("id-ID");}
-function showModal(text,callback){
- const modal=document.getElementById("modal");
- document.getElementById("modal-text").innerText=text;
- modal.classList.remove("hidden");
- document.getElementById("modal-confirm").onclick=()=>{modal.classList.add("hidden");callback();};
- document.getElementById("modal-cancel").onclick=()=>{modal.classList.add("hidden");};
-}
